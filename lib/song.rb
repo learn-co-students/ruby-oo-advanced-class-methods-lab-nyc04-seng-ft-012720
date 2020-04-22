aad10ceed513fb8acae51f7d33101cc4d816415f
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-04-21 22:32:02 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,80 +1,86 @@
 require "pry"
 
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  # def initialize
-  #   @song = song 
-  #   @name = name 
-  #   @artist_name = artist_name 
-  # end 
+  attr_accessor :name, :artist_name 
+  @@all = [] 
 
   def self.all
-    @@all
+    @@all 
   end
 
   def save
-    self.class.all << self
-  end
+    self.class.all << self  
+  end 
 
   def self.create
-   song = self.new
-   @@all << song  
-   song   
+    song = self.new  
+    @@all << song 
+    song  
   end 
-
 
   def self.new_by_name(name)
-    song = self.new 
-    song.name = name 
-    song
+    new_song = self.new 
+    new_song.name = name  
+    new_song 
   end 
 
-  def self.create_by_name(name) 
-    song = self.new
-    song.name = name 
-    @@all << song 
-    song    
-   end 
-   
-  def self.find_by_name(name)
-    @@all.find{|a| a.name == name}
+  def self.create_by_name(name)
+    new_song = self.new 
+    new_song.name = name 
+    @@all << new_song
+    new_song  
   end 
-  
+
+  def self.find_by_name(name)
+    find_song = self.new
+    find_song.name = name 
+    @@all.find do |song|
+    song.name == name 
+    end 
+  end 
+
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
-  end 
+  end
 
   def self.alphabetical
-    @@all.sort_by do |a| a.name end    
-  end 
-
+   @@all.sort_by! do |song|
+    song.name 
+    end 
+   end 
+ 
   def self.new_from_filename(filename)
-    name_array = filename.split(' - ')
-    name_array[-1] = name_array[-1].split('.mp3')
-    name_array.flatten!
-    song = Song.new
-    song.name = name_array[-1]
-    song.artist_name = name_array[0]
-    song
-  end
+    filename = filename.delete_suffix(".mp3")
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1]
 
-  def self.create_from_filename(filename)
-    name_array = filename.split(' - ')
-    name_array[-1] = name_array[-1].split('.mp3')
-    name_array.flatten!
-    song = Song.new
-    song.name = name_array[-1]
-    song.artist_name = name_array[0]
-    song.save
-    song
-  end
+    song = self.new 
+    song.name = song_name 
+    song.artist_name = artist_name 
+    song 
+  end 
+ 
 
-     
+  def self.create_from_filename(name)
+    name = name.delete_suffix(".mp3")
+    parts = name.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1]
+
+    song = self.create 
+    song.name = song_name 
+    song.artist_name = artist_name 
+    song 
+  end 
 
   def self.destroy_all
-    @@all.clear 
-  end 
+  @@all.clear  
+  
+  end
 
-end
+end 
+
+ 
+
+
